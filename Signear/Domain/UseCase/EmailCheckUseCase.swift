4c0064a0a6db579783a368672e71de8c7a1f6b7c
홍필화 <hongpilhwa@hongpilhwaui-MacBookPro.local>
2021-05-09 22:58:11 +0900

    added UseCases

---
+++ Signear/Domain/UseCase/EmailCheckUseCase.swift
@@ -0,0 +1,19 @@
+//
+//  EmailCheckUseCase.swift
+//  signear
+//
+//  Created by 홍필화 on 2021/05/09.
+//
+
+import Foundation
+
+protocol EmailCheckUseCaseType {
+    func emailCheck(with email: String)
+}
+
+class EmailCheckUseCase: EmailCheckUseCaseType {
+    func emailCheck(with email: String) {
+        // TODO
+    }
+    
+}
---
+++ Signear/Domain/UseCase/SignUpUseCase.swift
@@ -0,0 +1,19 @@
+//
+//  SignUpUseCase.swift
+//  signear
+//
+//  Created by 홍필화 on 2021/05/09.
+//
+
+import Foundation
+
+protocol SignUpUseCaseType {
+    func signUp(with email: String, with password: String, with mobile: String)
+}
+
+class SignUpUseCase: SignUpUseCaseType {
+    func signUp(with email: String, with password: String, with mobile: String) {
+        // TODO
+    }
+    
+}
