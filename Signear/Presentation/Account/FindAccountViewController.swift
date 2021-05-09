4b14d7e19641f5f1114dc9afa976c78996225882
jayce1116 <jaycesub@gmail.com>
2021-05-09 19:11:55 +0900

    add findAccount vc, vm

---
+++ Signear/Presentation/Account/FindAccountViewController.swift
@@ -0,0 +1,38 @@
+//
+//  FindAccountViewController.swift
+//  signear
+//
+//  Created by 신정섭 on 2021/05/09.
+//
+
+import UIKit
+
+class FindAccountViewController: UIViewController {
+    
+    // MARK : UI Life Cycle
+    
+    override func viewDidLoad() {
+        initUI()
+    }
+}
+
+// MARK : Private
+
+extension FindAccountViewController {
+    
+    private func initUI() {
+        initBackgroundColor()
+    }
+    
+    private func initBackgroundColor() {
+        let gradientLayer = CAGradientLayer()
+        gradientLayer.frame = self.view.bounds
+        gradientLayer.colors = [UIColor.init(r: 10, g: 132, b: 255).cgColor,
+                                UIColor.init(r: 0, g: 178, b: 255).cgColor]
+        self.view.layer.insertSublayer(gradientLayer, at: 0)
+    }
+    
+    private func bindUI() {
+        
+    }
+}
